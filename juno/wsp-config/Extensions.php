8b5dc4d4510182efc9e3afef638b5482f239b284
Galahad <galahad@wikisp.org>
2022-03-02 00:14:00 -0400

    Comment Counter. Per T296026

--- juno/wsp-config/Extensions.php
+++ juno/wsp-config/Extensions.php
@@ -24,7 +24,7 @@ wfLoadExtension( 'Echo' );
 wfLoadExtension( 'Babel' );
 wfLoadExtension( 'cldr' );
 wfLoadExtension( 'UniversalLanguageSelector' );
-wfLoadExtension ( 'Counter' );
+#wfLoadExtension ( 'Counter' );
 wfLoadExtension( 'TabberNeue' );
 
 # Flow & ParserFunctions need to be instaled both
