9abb31a2c8cb642cf3e94569f8325810d0f446c9
Galahad <galahad@wikisp.org>
2022-10-09 01:21:03 -0400

    Modif. archivo de act

--- update.sh
+++ update.sh
@@ -19,7 +19,7 @@ unzip mediawiki-*.zip
 
 echo Cambiando el nombre de la carpeta y actualizando repositorio de wikisp
 
-mv mediawiki-* wiki2
+mv mediawiki-1.38.4 wiki2
 cd mediawiki-config
 git pull
 cd ..
@@ -27,11 +27,13 @@ cd ..
 echo Tomaré un cafecito...
 sleep 5s
 echo Listo! Copiando LocalSettings.php y wsp-config a wiki2. Moviendo wiki2 a wikisp
-
-cp mediawiki-config/juno/wiki/LocalSettings.php wiki2/LocalSettings.php
+cp mediawiki-config/juno/LocalSettings.php wiki2/LocalSettings.php
+cp mediawiki-config/juno/Extensions.php wiki2/Extensions.php
+cp mediawiki-config/juno/UserGroups.php wiki2/UserGroups.php
+cp mediawiki-config/juno/Namespaces.php wiki2/Namespaces.php
 
 sudo -s
-mv wiki2 /var/www/wikisp/
+mv wiki2 /var/www/wikisp/wiki2
 
 echo Tomaré otro cafecito...
 sleep 5s
@@ -43,6 +45,7 @@ cp /var/www/wikisp/wiki/.htaccess /var/www/wikisp/wiki2/.htaccess
 echo Moviéndonos a carpeta extensiones y descargando
 cd /var/www/wikisp/wiki2/extensions
 git clone https://gerrit.wikimedia.org/r/mediawiki/extensions/CheckUser --branch REL1_38
+git clone https://gerrit.wikimedia.org/r/mediawiki/extensions/Echo --branch REL1_38
 
 echo Tomaré otro cafecito...
 sleep 5s
@@ -60,15 +63,14 @@ echo Segumos descargando...
 git clone https://gerrit.wikimedia.org/r/mediawiki/extensions/Babel --branch REL1_38
 git clone https://gerrit.wikimedia.org/r/mediawiki/extensions/CharInsert --branch REL1_38
 git clone https://gerrit.wikimedia.org/r/mediawiki/extensions/intersection --branch REL1_38
-git clone https://gerrit.wikimedia.org/r/mediawiki/extensions/TabberNeue --branch REL1_38
 git clone https://gerrit.wikimedia.org/r/mediawiki/extensions/TemplateStyles --branch REL1_38
 
 echo Nos tomamos un pequeño descanso...
 sleep 5s
 echo Seguimos descargando...
 
-git clone https://github.com/sagamusix/MediaWiki-Counter.git Counter --branch REL1_38
-git clone https://github.com/StarCitizenTools/mediawiki-extensions-TabberNeue.git TabberNeue --branch REL1_38
+git clone https://github.com/sagamusix/MediaWiki-Counter.git Counter
+git clone https://github.com/StarCitizenTools/mediawiki-extensions-TabberNeue.git TabberNeue
 git clone https://gerrit.wikimedia.org/r/mediawiki/extensions/cldr --branch REL1_38
 git clone https://gerrit.wikimedia.org/r/mediawiki/extensions/CleanChanges --branch REL1_38
 git clone https://gerrit.wikimedia.org/r/mediawiki/extensions/Flow --branch REL1_38
@@ -78,13 +80,16 @@ echo Nos tomamos un pequeño descanso...
 sleep 5s
 echo Seguimos descargando...
 
-git clone https://gerrit.wikimedia.org/r/mediawiki/extensions/PluggableAuth --branch REL1_38
+git clone https://gerrit.wikimedia.org/r/mediawiki/extensions/PluggableAuth
 git clone https://gerrit.wikimedia.org/r/mediawiki/extensions/SecurePoll --branch REL1_38
 git clone https://gerrit.wikimedia.org/r/mediawiki/extensions/StaffEdits --branch REL1_38
 git clone https://gerrit.wikimedia.org/r/mediawiki/extensions/StaffPowers --branch REL1_38
 git clone https://gerrit.wikimedia.org/r/mediawiki/extensions/UniversalLanguageSelector --branch REL1_38
-git clone https://gerrit.wikimedia.org/r/mediawiki/extensions/WSOAuth --branch REL1_38
+git clone https://gerrit.wikimedia.org/r/mediawiki/extensions/WSOAuth
 #git clone https://gerrit.wikimedia.org/r/mediawiki/extensions/ContactPage --branch REL1_38
+
+echo Eliminando extensiones innecesarias
+rm -rf AbuseFilter CiteThisPage ConfirmEdit ImageMap Math PageImages MultimediaViewer SecureLinkFixer Nuke OATHAuth PdfHandler ReplaceText TextExtracts TitleBlacklist VisualEditor
 
 echo Nos tomamos un pequeño descanso...
 sleep 5s
@@ -103,8 +108,8 @@ composer install --no-dev
 sleep 5s
 echo Cambiando lugares
 
-#mv wiki oldwiki
-#mv wiki2 wiki
+#mv /var/www/wikisp/wiki /var/www/wikisp/oldwiki
+#mv /var/www/wikisp/wiki2 /var/www/wikisp/wiki
 
 #echo Lugares cambiados
 #echo Iniciando actualización de base de datos
